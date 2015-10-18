9975fae381e4e8e95eb5ef31419d264525f58d8a
Mathew Kurian <MathewKurian@utexas.edu>
2015-10-18 04:32:22 -0500

    Prepping for rackspace

--- index.py
+++ index.py
@@ -3,14 +3,37 @@
 
 
 from flask import Flask, render_template
+
 app = Flask(__name__, static_folder='public', static_url_path='/assets')
 
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 app.debug = True
 
+
 @app.route('/')
-def splash():
+def index():
     return render_template('index.jade')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.jade')
+
+
+@app.route('/games')
+def games():
+    return render_template('games.jade')
+
+
+@app.route('/releases')
+def releases():
+    return render_template('releases.jade')
+
+
+@app.route('/publishers')
+def publishers():
+    return render_template('publishers.jade')
+
 
 if __name__ == '__main__':
     app.run()
