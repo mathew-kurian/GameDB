b372d435bad28fe2567ea60a60e193a137756a2a
Matthew Kim <myk235@cs.utexas.edu>
2015-10-22 15:22:10 -0500

    fixed some unit tests

--- tests.py
+++ tests.py
@@ -29,23 +29,25 @@ class TestGames (TestCase) :
         game = Game("Halo", "Shooter", "Xbox", "Bungie Studios", "Havok", "tag1", 4.5)
         self.assertEqual(game.__name__, "Halo")
     def test_game2 (self) :
-        game = Game("Halo", "Shooter", "Xbox", "Bungie Studios", "Havok", "tag1", 4.5)
-        self.assertEqual(game.__name__, "Halo")
+        game = Game("Smashy Road", "Driving", "IOS", "RK Games", "Some Engine", "tag1", 4.5)
+        self.assertEqual(game.company.__name__, "RK Games")
+        self.assertEqual(game.__name__, "Smashy Road")
     def test_game3 (self) :
-        game = Game("Halo", "Shooter", "Xbox", "Bungie Studios", "Havok", "tag1", 4.5)
+        game = Game("Super Mario Bros.", "Platformer", "Ancient Computer", "Nintendo", "Ancient Engine", "tag1", 4.5)
+        self.assertEqual(game.platform.__name__, "Ancient Computer")
         self.assertEqual(game.__name__, "Halo")
     # ----
     # Developer
     # ----
 
     def test_company1 (self) :
-        company = Company("Nintendo", "Japan", "games", 1950, "CEO man", "COO man", 5000)
+        company = Company("Nintendo", "Japan", "Nintendo Games", 1950, "Mr. Nintendo", "Dr. Nintendo", 5000)
         self.assertEqual(company.__name__, "Nintendo")
     def test_company2 (self) :
-        company = Company("Nintendo", "Japan", "games", 1950, "CEO man", "COO man", 5000)
+        company = Company("Sega", "Japan", "Sega Games", 1960, "Mr. Sega", "Mrs. Sega", 4000)
         self.assertEqual(company.__name__, "Nintendo")
     def test_company3 (self) :
-        company = Company("Nintendo", "Japan", "games", 1950, "CEO man", "COO man", 5000)
+        company = Company("EA Games", "America", "EA games", 1970, "Mr. EA", "Mrs. EA", 3000)
         self.assertEqual(company.__name__, "Nintendo")
     # ----
     # Platform
@@ -55,11 +57,11 @@ class TestGames (TestCase) :
         platform = Platform("2000", 4.5, "PS2", 1000000, 2)
         self.assertEqual(platform.__name__, "PS2")
     def test_platform2 (self) :
-        platform = Platform("2000", 4.5, "PS2", 1000000, 2)
-        self.assertEqual(platform.__name__, "PS2")
+        platform = Platform("2000", 4.5, "GameCube", 1000000, 2)
+        self.assertEqual(platform.__name__, "GameCube")
     def test_platform3 (self) :
-        platform = Platform("2000", 4.5, "PS2", 1000000, 2)
-        self.assertEqual(platform.__name__, "PS2")
+        platform = Platform("2000", 4.5, "Xbox", 1000000, 2)
+        self.assertEqual(platform.__name__, "Xbox")
 
 # ----
 # main
