74e6700b1f0919fc91e87e63922d25f9f41b8197
MatthewKimUT <matt.kim@utexas.edu>
2015-10-22 19:14:00 -0500

    Added more depth to unit tests (querying)

--- tests.py
+++ tests.py
@@ -10,12 +10,14 @@ from unittest import main, TestCase
 from models import Game, Developer, Platform, Image
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = '' #insert URI
 db = SQLALchemy(app)
-
+Session = sessionmaker()
+session = Session()
 # -----------
 # Test Games
 # -----------
@@ -27,14 +29,25 @@ class TestGames (TestCase) :
 
     def test_game1 (self) :
         game = Game("Halo", "2001")
+        session.add(game)
+        session.commit()
         self.assertEqual(game.__name__, "Halo")
     def test_game2 (self) :
         args = {"developers":["RK Games"], "platforms":["IOS", "Android"]}
         game = Game("Smashy Road", "2014", args)
+        session.add(game)
+        session.commit()
         self.assertEqual(game.__name__, "Smashy Road")
     def test_game3 (self) :
         args = {"developers":["Nintendo"], "platforms":["Some Ancient Console"]}
         game = Game("Super Mario Bros.", "1985", args)
+        session.add(game)
+        session.commit()
+        for instance in session.query(Game).order_by(Game.id): 
+            if(instance.name == "Smashy Road"):
+                self.assertEqual(instance.platforms, ["IOS", "Android"])
+            if(instance.name == "Super Mario Bros."):
+                self.assertEqual(instance.developers, ["Nintendo"])
         self.assertEqual(game.__name__, "Halo")
     # ----
     # Developer
@@ -42,14 +55,25 @@ class TestGames (TestCase) :
 
     def test_company1 (self) :
         company = Company("Nintendo", "address", "city", "state", "Japan", "123-456-7890", "nintendo.com", "It's nintendo")
+        session.add(company);
+        session.commit()
         self.assertEqual(company.__name__, "Nintendo")
     def test_company2 (self) :
         args = {"developed_games": ["Sonic"], "published_games":["Also Sonic?"]}
         company = Company("Sega", "address", "city", "state", "Japan", "123-456-7890", "sega.com", "It's sega", args)
+        session.add(company);
+        session.commit()
         self.assertEqual(company.__name__, "Sega")
     def test_company3 (self) :
         args = {"developed_games":["Sports simulator 2015", "Another Sports simluator 2014"], "published_games":["the Sims 1", "The Sims 2-4"]}
         company = Company("EA Games", "address", "city", "state", "USA", "123-456-7890", "EA.com", "It's EA")
+        session.add(company);
+        session.commit()
+        for instance in session.query(Company).order_by(Company.id): 
+            if(instance.name == "Sega"):
+                self.assertEqual(instance.developed_games, ["Sonic"])
+            if(instance.name == "EA Games"):
+                self.assertEqual(instance.published_games, ["the Sims 1", "The Sims 2-4"])
         self.assertEqual(company.__name__, "EA Games")
     # ----
     # Platform
@@ -57,14 +81,25 @@ class TestGames (TestCase) :
 
     def test_platform1 (self) :
         platform = Platform("2000", "PS2", 400, False, "asdf", "asdf")
+        session.add(platform)
+        session.commit()
         self.assertEqual(platform.__name__, "PS2")
     def test_platform2 (self) :
         args = {"games":["Super Smash Bros. Melee", "Mario Party 5"]}
         platform = Platform("2000", "GameCube", 300, False, "asdf", "asdf", args)
+        session.add(platform)
+        session.commit()
         self.assertEqual(platform.__name__, "GameCube")
     def test_platform3 (self) :
         args = {"games":["Halo", "Star Wars Battlefront 2", "Call of Duty: Modern Warfare"], "companies":["Bungie", "Infinity Ward"]}
         platform = Platform("2000", "Xbox", 300, False, "asdf", "asdf", args)
+        session.add(platform)
+        session.commit()
+        for instance in session.query(Platform).order_by(Platform.id): 
+            if(instance.name == "GameCube"):
+                self.assertEqual(instance.games, ["Super Smash Bros. Melee", "Mario Party 5"])
+            if(instance.name == "EA Games"):
+                self.assertEqual(instance.companies, ["Bungie", "Infinity Ward"])
         self.assertEqual(platform.__name__, "Xbox")
 
 # ----
