90165a6078bb774df4ceb0fa30bfc1df57428011
Mathew Kurian <MathewKurian@utexas.edu>
2015-10-30 02:34:57 -0500

    Adding loglines

--- migrate.py
+++ migrate.py
@@ -3,7 +3,10 @@
 from db import *
 
 if __name__ == "__main__":
+    print('started migration')
     session = get_session(echo=False)
     migrate(session)
     session.commit()
     session.close()
+    print('finished migration')
+    exit(0)
