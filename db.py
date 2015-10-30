c64e2ab6b78150f003cd928d642acb7dfa0f6f11
Mathew Kurian <MathewKurian@utexas.edu>
2015-10-30 01:21:37 -0500

    wtfffff

--- db.py
+++ db.py
@@ -41,82 +41,75 @@ def migrate(session=None):
     Base.metadata.create_all(session.bind)
 
     with open('scripts/giantbomb/csv/games.json') as f:
-        for i in json.load(f):
-            session.add(Game(**{
-                'id': i["id"],
-                'name': i["name"],
-                'rating': i["rating"],
-                'release_date': date_cast(i["release_date"]),
-                'deck': i["deck"],
-                'concepts': i["concepts"],
-                'genres': i["genres"],
-                'franchises': i["franchises"],
-                'description': i["description"]
-            }))
+        session.add_all([Game(**{
+            'id': i["id"],
+            'name': i["name"],
+            'rating': i["rating"],
+            'release_date': date_cast(i["release_date"]),
+            'deck': i["deck"],
+            'concepts': i["concepts"],
+            'genres': i["genres"],
+            'franchises': i["franchises"],
+            'description': i["description"]
+        }) for i in json.load(f)])
 
     with open('scripts/giantbomb/csv/companies.json') as f:
-        for i in json.load(f):
-            session.add(Company(**{
-                'id': i['id'],
-                'name': i['name'],
-                'founded_date': date_cast(i['founded_date']),
-                'address': i['address'],
-                'city': i['city'],
-                'country': i['country'],
-                'state': i['state'],
-                'deck': i['deck'],
-                'concepts': i['concepts'],
-                'phone': i['phone'],
-                'website': i['website'],
-                'description': i['description']
-            }))
+        session.add_all([Company(**{
+            'id': i['id'],
+            'name': i['name'],
+            'founded_date': date_cast(i['founded_date']),
+            'address': i['address'],
+            'city': i['city'],
+            'country': i['country'],
+            'state': i['state'],
+            'deck': i['deck'],
+            'concepts': i['concepts'],
+            'phone': i['phone'],
+            'website': i['website'],
+            'description': i['description']
+        }) for i in json.load(f)])
 
     with open('scripts/giantbomb/csv/platforms.json') as f:
-        for i in json.load(f):
-            session.add(Platform(**{
-                'id': i['id'],
-                'name': i['name'],
-                'release_date': date_cast(i['release_date']),
-                'online_support': i['online_support'],
-                'price': i['price'],
-                'company': i['company'],
-                'deck': i['deck'],
-                'install_base': i['install_base'],
-                'description': i['description']
-            }))
+        session.add_all([Platform(**{
+            'id': i['id'],
+            'name': i['name'],
+            'release_date': date_cast(i['release_date']),
+            'online_support': i['online_support'],
+            'price': i['price'],
+            'company': i['company'],
+            'deck': i['deck'],
+            'install_base': i['install_base'],
+            'description': i['description']
+        }) for i in json.load(f)])
 
     with open('scripts/giantbomb/csv/games-companies.json') as f:
-        for i in json.load(f):
-            session.add(GameCompany(**{
-                'game_id': i['game'],
-                'company_id': i['company'],
-                'role': i['relation']
-            }))
+        session.add_all([GameCompany(**{
+            'game_id': i['game'],
+            'company_id': i['company'],
+            'role': i['relation']
+        }) for i in json.load(f)])
 
     with open('scripts/giantbomb/csv/games-platforms.json') as f:
-        for i in json.load(f):
-            session.add(GamePlatform(**{
-                'platform_id': i['platform'],
-                'game_id': i['game']
-            }))
+        session.add_all([GamePlatform(**{
+            'platform_id': i['platform'],
+            'game_id': i['game']
+        }) for i in json.load(f)])
 
     with open('scripts/giantbomb/csv/images.json') as f:
-        for i in json.load(f):
-            session.add(Url(**{
-                'entity_id': i['id'],
-                'entity': i['relation'],
-                'type': 'image',
-                'source': i['image']
-            }))
+        session.add_all([Url(**{
+            'entity_id': i['id'],
+            'entity': i['relation'],
+            'type': 'image',
+            'source': i['image']
+        }) for i in json.load(f)])
 
     with open('scripts/giantbomb/csv/videos.json') as f:
-        for i in json.load(f):
-            session.add(Url(**{
-                'entity_id': i['id'],
-                'entity': i['relation'],
-                'type': 'video',
-                'source': i['video']
-            }))
+        session.add_all([Url(**{
+            'entity_id': i['id'],
+            'entity': i['relation'],
+            'type': 'video',
+            'source': i['video']
+        }) for i in json.load(f)])
 
     if close:
         session.commit()
--- migrate.py
+++ migrate.py
@@ -1,7 +1,7 @@
 from db import *
 
 if __name__ == "__main__":
-    session = get_session(echo=True)
+    session = get_session(echo=False)
     migrate(session)
     session.commit()
     session.close()
