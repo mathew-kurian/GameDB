3ad00a5b81fa0f1f6b1cd657aa44187ae3340b3d
Rahul Rajavel <rrahul@msn.com>
2015-11-16 12:01:48 -0600

    fixed typo

--- index.py
+++ index.py
@@ -119,7 +119,7 @@ def api_search(name, index = 0):
             entity = session.query(table).get(result['id'])
             if entity is None:
                 continue
-            res['results'] += [[result['name'], result['id'], result['deck'], result['description']s, entity.images[0]]]
+            res['results'] += [[result['name'], result['id'], result['deck'], result['description'], entity.images[0]]]
             res['status'] = 0
             counted += 1
 
@@ -163,4 +163,4 @@ def add_header(response):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=args.port)
+    app.run(host='0.0.0.0', port=args.port + 1)
