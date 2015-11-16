c263a9b4d951358141295e07a4845752e5f6054a
Rahul Rajavel <rrahul@msn.com>
2015-11-16 13:12:09 -0600

    json output

--- index.py
+++ index.py
@@ -119,12 +119,19 @@ def api_search(name, index = 0):
             if entity is None:
                 continue
 
-            #check if these fields exist
-            deck = result['deck'][0] if 'deck' in result else 'No Deck'
-            description = result['description'][0] if 'description' in result else 'No Description'
-            images = entity.images[0] if 'images' in result and len(entity.images) > 0 else 'No Images'
+            result = to_dict(result)
+            for i in result:
+                result[i] = result[i][0] if len(result[i]) > 0 else 'Nothing'
 
-            res['results'] += [[result['name'][0], result['id'], deck, description, images]]
+            result['images'] = to_dict(entity.images)
+
+            #check if these fields exist
+            # deck = result['deck'][0] if 'deck' in result else 'No Deck'
+            # description = result['description'][0] if 'description' in result else 'No Description'
+            # images = entity.images[0] if 'images' in result and len(entity.images) > 0 else 'No Images'
+
+            # res['results'] += [[result['name'][0], result['id'], deck, description, images]]
+            res['results'] += result
             res['status'] = 0
             counted += 1
 
