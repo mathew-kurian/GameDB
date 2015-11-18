bd42e58ca6f1a13e74b09635bf6a70056a690fc8
Rahul Rajavel <rrahul@msn.com>
2015-11-17 23:28:21 -0600

    solr start script final

--- solr_start.py
+++ solr_start.py
@@ -3,3 +3,19 @@ import subprocess
 p = subprocess.Popen("make solr-start", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 while p.poll() is None:
 	continue
+
+print('STDOUT')
+if p.stdout is None:
+	print('-')
+else:
+	for line in p.stdout:
+		print(line.decode("utf-8"), end = "")
+
+print('\nSTDERR')
+if p.stderr is None:
+	print('-')
+else:
+	for line in p.stderr:
+		print(line.decode("utf-8"), end = "")
+
+print('\n')
